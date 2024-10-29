f94460451b8fc6a06777ec363687df7a56c95518
andrewtigba <128866259+andrewtigba@users.noreply.github.com>
2024-10-29 11:06:53 +0800

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,54 @@
 const fs = require('fs');
 const path = require('path');
+const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'help',
   description: 'Show available commands',
+  usage: 'help\nhelp [command name]',
   author: 'System',
-  execute(senderId, args, pageAccessToken, sendMessage) {
+  execute(senderId, args, pageAccessToken) {
     const commandsDir = path.join(__dirname, '../commands');
     const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
 
+    if (args.length > 0) {
+      const commandName = args[0].toLowerCase();
+      const commandFile = commandFiles.find(file => {
+        const command = require(path.join(commandsDir, file));
+        return command.name.toLowerCase() === commandName;
+      });
+
+      if (commandFile) {
+        const command = require(path.join(commandsDir, commandFile));
+        const commandDetails = `
+﹌﹌﹌﹌﹌﹌﹌﹌
+𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝙽𝚊𝚖𝚎: ${command.name}
+𝙳𝚎𝚜𝚌𝚛𝚒𝚋𝚝𝚒𝚘𝚗: ${command.description}
+𝚄𝚜𝚊𝚐𝚎: ${command.usage}
+﹌﹌﹌﹌﹌﹌﹌﹌`;
+        
+        sendMessage(senderId, { text: commandDetails }, pageAccessToken);
+      } else {
+        sendMessage(senderId, { text: `Command "${commandName}" not found.` }, pageAccessToken);
+      }
+      return;
+    }
+
     const commands = commandFiles.map(file => {
       const command = require(path.join(commandsDir, file));
-      return `⟿ ${command.name}\n  - ${command.description}\n  - Credits: ${command.author}`;
+      return `│ - ${command.name}`;
     });
 
-    const totalCommands = commandFiles.length;
-    const helpMessage = `Here are the available commands: \nTotal commands: ${totalCommands} \n\n${commands.join('\n\n')}`;
-    
+    const helpMessage = `
+━━━━━━━━━━━━━━
+𝙰𝚟𝚊𝚒𝚕𝚊𝚋𝚕𝚎 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜:
+╭─╼━━━━━━━━╾─╮
+${commands.join('\n')}
+╰─━━━━━━━━━╾─╯
+Chat -help [name] 
+to see command details.
+━━━━━━━━━━━━━━`;
+
     sendMessage(senderId, { text: helpMessage }, pageAccessToken);
   }
 };
